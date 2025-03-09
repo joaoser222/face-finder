8480b0757b1130b69d6da9eb3d77c4c5e08f91ca
joaoser222 <joaoser222@gmail.com>
2025-03-09 11:25:24 -0300

    Adc. verificação de segurança nos controllers

--- api/app/controllers/base_controller.py
+++ api/app/controllers/base_controller.py
@@ -1,14 +1,18 @@
-from typing import List
-from fastapi import HTTPException
-from fastapi import APIRouter
+from fastapi import HTTPException, Depends, APIRouter, Request
+from app.utils import get_current_user
+from app.models.user import User
 
 class BaseController:
     model = None
     prefix = ""
+    current_user = None  # Atributo para armazenar o usuário autenticado
 
     def __init__(self):
         # Cria o router com o prefixo dinâmico
         self.router = APIRouter(prefix=f"/{self.prefix}", tags=[self.prefix])
+
+        # Adiciona uma dependência global para o roteador
+        self.router.dependencies.append(Depends(self.set_current_user))
 
         # Define as rotas
         self.router.add_api_route("/list", self.get_all, methods=["GET"])
@@ -16,25 +20,39 @@ class BaseController:
         self.router.add_api_route("/create", self.create, methods=["POST"])
         self.router.add_api_route("/delete/{id}", self.delete, methods=["DELETE"])
 
+    async def set_current_user(self, user: User = Depends(get_current_user)):
+        # Armazena o usuário autenticado no atributo da classe
+        self.current_user = user
+
     async def get_all(self):
-        return await self.model.all()
+        try:
+            return await self.model.all()
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
     async def show(self, id: int):
-        record = await self.model.get_or_none(id=id)
-        if not record:
-            raise HTTPException(status_code=404, detail="Registro não encontrado")
-        return record
+        try:
+            record = await self.model.get_or_none(id=id)
+            if not record:
+                raise HTTPException(status_code=404, detail="Registro não encontrado")
+            return record
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
     async def create(self, params: dict):
         try:
+            params["user_id"] = self.current_user.id
             record = await self.model.create(**params)
             return record
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
 
     async def delete(self, id: int):
-        record = await self.model.get_or_none(id=id)
-        if not record:
-            raise HTTPException(status_code=404, detail="Registro não encontrado")
-        await record.delete()
-        return {"message": "Registro deletado com sucesso"}
+        try:
+            record = await self.model.get_or_none(id=id)
+            if not record:
+                raise HTTPException(status_code=404, detail="Registro não encontrado")
+            await record.delete()
+            return {"message": "Registro deletado com sucesso"}
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
