9dc7fd05b482a1c2cf3458576b5060c9550e7dfe
joaoser222 <joaoser222@gmail.com>
2025-03-09 12:56:36 -0300

    Corr. configuração de variáveis de ambiente

--- api/app/config.py
+++ api/app/config.py
@@ -1,31 +1,43 @@
 from tortoise import Tortoise
 import os
+from dotenv import load_dotenv
 
-# Obtém a URL do banco de dados das variáveis de ambiente
-DATABASE_POSTGRES_URL = os.getenv("DATABASE_POSTGRES_URL")
+# Carrega as variáveis de ambiente do .env
+load_dotenv(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".env")))
+            
 
 # Verifica se a URL do banco de dados foi definida
-if not DATABASE_POSTGRES_URL:
-    raise ValueError("A variável de ambiente não está definida.")
+if not os.getenv("DATABASE_POSTGRES_URL"):
+    raise ValueError("A variável de ambiente DATABASE_POSTGRES_URL não está definida.")
+
+# Configuração do Tortoise ORM
+TORTOISE_ORM = {
+    "connections": {
+        "default": os.getenv("DATABASE_POSTGRES_URL"),  # Usa a URL do banco de dados da variável de ambiente
+    },
+    "apps": {
+        "models": {
+            "models": [
+                "app.models.user", 
+                "app.models.collection", 
+                "app.models.photo", 
+                "app.models.face", 
+                "app.models.search", 
+                "app.models.search_collection",
+                "app.models.session",
+                "aerich.models",
+            ],
+            "default_connection": "default",
+        }
+    }
+}
 
 async def init_db():
     """
     Inicializa o banco de dados usando o Tortoise ORM.
     """
-    await Tortoise.init(
-        db_url=DATABASE_POSTGRES_URL,
-        modules={
-            'models': [
-                'app.models.user', 
-                'app.models.collection', 
-                'app.models.photo', 
-                'app.models.face', 
-                'app.models.search', 
-                'app.models.search_collection',
-                'app.models.session'
-            ]
-        }
-    )
+    await Tortoise.init(config=TORTOISE_ORM)
+
     # Gera os esquemas do banco de dados (cria as tabelas, se não existirem)
     await Tortoise.generate_schemas()
 
