987766fc1999e1edf4b59cc3775956a4e3fc78b5
joaoser222 <joaoser222@gmail.com>
2025-04-05 18:24:38 -0300

    Rem. validação não utilizada

--- client/src/plugins/validations.js
+++ client/src/plugins/validations.js
@@ -1,7 +1,6 @@
 
 export default {
   required: (value) => !!value || "Este campo é obrigatório",
-  required_with: (value) => !!value && !!this.data[param] || "Este campo é obrigatório",
   email: (value) =>(!value || /.+@.+\..+/.test(value)) || "Este campo deve ser um email válido",
   minLength: (length) => (value) => value.length >= length || `Este campo deve ter pelo menos ${length} caracteres`,
   maxLength: (length) => (value) => value.length <= length || `Este campo deve ter no máximo ${length} caracteres`,
