6f252040c4cffae803ca4b73bb57a11a354cef7c
joaoser222 <joaoser222@gmail.com>
2025-04-05 18:24:20 -0300

    Adc. maska nas dependências

--- client/src/plugins/index.js
+++ client/src/plugins/index.js
@@ -7,8 +7,10 @@
 // Plugins
 import vuetify from './vuetify'
 import router from '@/router'
+import { vMaska } from "maska/vue";
 
 export function registerPlugins (app) {
+  app.directive('maska',vMaska);
   app
     .use(vuetify)
     .use(router)
---
+++ client/src/plugins/masks.js
@@ -0,0 +1,8 @@
+export default {
+    uppercase: {
+        preProcess: (v)=>v.toUpperCase(),
+    },
+    lowercase: {
+        preProcess: (v)=>v.toLowerCase(),
+    }
+}
