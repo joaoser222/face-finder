05ac246d090bc0176e1f636c1316ae87a0f37b7f
joaoser222 <joaoser222@gmail.com>
2025-04-06 09:25:48 -0300

    Adc. campo de status e photo_quantity em collections

--- api/app/models/collection.py
+++ api/app/models/collection.py
@@ -4,6 +4,8 @@ class Collection(models.Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=100)
     user = fields.ForeignKeyField('models.User', related_name='collections')
+    status = fields.IntField(default=0)
+    photo_quantity = fields.IntField(default=0)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now_add=True)
 
