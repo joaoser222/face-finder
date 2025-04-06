2c2e0f9293777921a2a93a9e20137d62b2695f6b
joaoser222 <joaoser222@gmail.com>
2025-04-06 09:28:11 -0300

    Adc. tabela de queues para gerenciar processos em segundo plano

---
+++ api/app/models/queue.py
@@ -0,0 +1,17 @@
+from tortoise import fields, models
+
+class Queue(models.Model):
+    id = fields.IntField(pk=True)
+    user = fields.ForeignKeyField('models.User', related_name='collections')
+    status = fields.IntField(default=0)
+    process_type = fields.CharField(max_length=50)
+    owner_type = fields.CharField(max_length=20)
+    owner_id = fields.IntField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "queues"
+
+    def __str__(self):
+        return str(self.id)
