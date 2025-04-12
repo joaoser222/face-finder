5770f273d863ed523b01a3179dd1b73cc3902289
joaoser222 <joaoser222@gmail.com>
2025-04-12 12:02:57 -0300

    Corr. funcionamento de componente de página

---
+++ client/src/components/BasePage.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="d-flex flex-column align-start justify-center w-100 h-100 py-3">
+    <div class="mb-3 w-100">
+      <h1>
+        <slot name="title"></slot>
+      </h1>
+    </div>
+    <div class="d-flex flex-row justify-end align-center my-4 w-100">
+      <slot name="action-buttons"></slot>
+    </div>
+    <v-card class="w-100 pa-3 flex-grow-1">
+      <v-card-text>
+        <div class="d-flex justify-space-between align-center mb-4" v-if="showSearch && !noItems">
+          <div class="pr-2 w-100">
+            <v-text-field 
+              v-model="search" 
+              label="Pesquisar" 
+              color="primary" 
+              hide-details 
+              single-line 
+              density="compact"
+              @keypress.enter="handleSearch"
+            >
+            <template #append-inner>
+              <v-btn 
+                icon="Search" 
+                density="comfortable"
+                color="primary"
+                variant="text"
+                rounded="circle" 
+                @click="handleSearch"
+              >
+              </v-btn>
+            </template>
+            </v-text-field>
+          </div>
+        </div>
+
+        <slot name="default"></slot>
+
+        <!-- Estado vazio -->
+        <v-empty-state v-if="noItems">
+          <template v-slot:media>
+            <div class="d-flex flex-column justify-center align-center">
+              <slot name="no-items-banner"></slot>
+            </div>
+          </template>
+
+          <template v-slot:title>
+            <div class="text-h6 mt-8">
+              <slot name="no-items-subtitle"></slot>
+            </div>
+          </template>
+
+          <template v-slot:text>
+            <div class="text-caption">
+              <slot name="no-items-description"></slot>
+            </div>
+          </template>
+        </v-empty-state>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'BasePage',
+  props:{
+    showSearch: {
+      type: Boolean,
+      default: true
+    },
+    showCreate: {
+      type: Boolean,
+      default: true
+    },
+    noItems: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    handleSearch() {
+      this.$emit('search', this.search)
+    }
+  }
+}
+</script>
