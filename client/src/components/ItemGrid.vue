2c09f07aca16b94b4ee0d950d0b67f10296277d6
joaoser222 <joaoser222@gmail.com>
2025-04-12 12:03:48 -0300

    Adc. componente para gerar grids de cards

---
+++ client/src/components/ItemGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row dense>
+    <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="item in items" :key="item.id">
+      <v-card>
+        <slot name="default" :item="item"></slot>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'ItemGrid',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
