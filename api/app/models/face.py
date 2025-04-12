e9004328e47fd6a9b1e16034b4dc5729949b6a8a
joaoser222 <joaoser222@gmail.com>
2025-04-12 16:59:01 -0300

    Corr. model de foto em faces

--- api/app/models/face.py
+++ api/app/models/face.py
@@ -4,7 +4,7 @@ from .base import BaseModel
 class Face(BaseModel):
     bbox = fields.JSONField()
     user = fields.ForeignKeyField('models.User', related_name='faces')
-    photo = fields.ForeignKeyField('models.File', related_name='faces')
+    photo = fields.ForeignKeyField('models.Photo', related_name='faces')
     collection = fields.ForeignKeyField('models.Collection', related_name='faces')
 
     class Meta:
