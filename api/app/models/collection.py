1ffe3e054ff07d1b366f75fe1ac6c1ad60fa6dd5
joaoser222 <joaoser222@gmail.com>
2025-04-12 12:07:23 -0300

    Adc. campo de thumbnail e validação unique name

--- api/app/models/collection.py
+++ api/app/models/collection.py
@@ -1,9 +1,11 @@
 from tortoise import fields, models
+from app.models.file import File
 
 class Collection(models.Model):
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=100)
+    name = fields.CharField(max_length=100, unique=True)
     user = fields.ForeignKeyField('models.User', related_name='collections')
+    thumbnail_file = fields.ForeignKeyField('models.File', related_name='collections', null=True)
     status = fields.IntField(default=0)
     photo_quantity = fields.IntField(default=0)
     created_at = fields.DatetimeField(auto_now_add=True)
