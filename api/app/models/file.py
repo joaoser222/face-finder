84ea1ee1006d276e844372ac59c3ebf730f01357
joaoser222 <joaoser222@gmail.com>
2025-04-12 12:07:51 -0300

    Adc. mime_type e user

--- api/app/models/file.py
+++ api/app/models/file.py
@@ -2,13 +2,16 @@ from tortoise import fields, models
 from typing import Any
 from app.utils import generate_unique_filename
 import os
+import mimetypes
 
 class File(models.Model):
     id = fields.IntField(pk=True)
     original_name = fields.CharField(max_length=500)
     file_path = fields.CharField(max_length=500)
     extension_type = fields.CharField(max_length=100)
+    mime_type = fields.CharField(max_length=100)
     size = fields.IntField()  # Tamanho em bytes
+    user = fields.ForeignKeyField('models.User', related_name='files')
     
     # Relações polimórficas (pode ser Collection, Search, etc.)
     owner_type = fields.CharField(max_length=20)  # 'collection', 'search', etc.
@@ -28,7 +31,7 @@ class File(models.Model):
     @classmethod
     async def create_file(
         cls,
-        owner: Any,  # Models relacionadas
+        owner: Any,  # Models relacionadas,
         file_name: str,
         file_size: int
     ) -> 'File':
@@ -36,11 +39,14 @@ class File(models.Model):
         try:
             owner_type = owner.__class__.__name__.lower()
             unique_filename = generate_unique_filename(file_name)
-            _,ext = os.path.splitext(unique_filename)
+            mime_type, _ = mimetypes.guess_type(unique_filename)
+            basename,ext = os.path.splitext(unique_filename)
             file = await cls.create(
+                user_id=owner.user_id,
                 original_name=file_name,
                 file_path=f"/app/files/{owner_type}/{owner.id}/{unique_filename}",
                 extension_type=ext,
+                mime_type=mime_type,
                 size=file_size,
                 owner_type=owner_type,
                 owner_id=owner.id,
