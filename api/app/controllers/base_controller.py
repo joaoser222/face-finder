3fac0920eb99d61ef936294b3ece817ec109323b
joaoser222 <joaoser222@gmail.com>
2025-04-12 11:21:24 -0300

    Rem. referencia de model do BaseController

--- api/app/controllers/base_controller.py
+++ api/app/controllers/base_controller.py
@@ -3,13 +3,13 @@ from app.utils import get_current_user
 from app.models.user import User
 
 class BaseController:
-    model = None
     prefix = ""
     current_user = None  # Atributo para armazenar o usuário autenticado
 
     def __init__(self):
         # Cria o router com o prefixo dinâmico
         self.router = APIRouter(prefix=f"/{self.prefix}", tags=[self.prefix])
+    
 
     async def set_current_user(self, user: User = Depends(get_current_user)):
         # Armazena o usuário autenticado no atributo da classe
