6fd4f2ddcb7db288dcf4b59dc665e0a322e8d906
joaoser222 <joaoser222@gmail.com>
2025-04-12 12:06:10 -0300

    Corr. filtragem e paginação do controller ViewController

--- api/app/controllers/view_controller.py
+++ api/app/controllers/view_controller.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, Depends, UploadFile
+from fastapi import HTTPException, Depends, UploadFile,Query
 from fastapi.responses import JSONResponse
 from app.utils import logger_info,logger_error
 from typing import Any
@@ -12,6 +12,7 @@ class ViewController(BaseController):
     model = None
     prefix = ""
     current_user = None  # Atributo para armazenar o usuário autenticado
+    search_field = ""
 
     def __init__(self):
         super().__init__()
@@ -26,18 +27,43 @@ class ViewController(BaseController):
         self.router.add_api_route("/update/{id}", self.update, methods=["PUT"])
         self.router.add_api_route("/delete/{id}", self.delete, methods=["DELETE"])
     
-    def __get_model_by_user(self):
+    def get_model_by_user(self):
         """
         Filtra os registros da model pelo usuário autenticado
         """
         return self.model.filter(user_id=self.current_user.id)
+    
+    async def query_paginator(self, query, page):
+        """
+        Retorna os registros da model com paginação
+        """
+        try:
+            per_page = 24
+            total = await query.count()
+            items = await query.offset((page - 1) * per_page).limit(per_page)
+            
+            return {
+                "data": items,
+                "total": total,
+                "page": page,
+                "per_page": per_page,
+                "total_pages": (total + per_page - 1) // per_page
+            }
+        except Exception as e:
+            logger_error(__name__,e)
+            raise
 
-    async def get_all(self):
+
+    async def get_all(self,search: str = Query(None),page: int = Query(1, ge=1)):
         """
         Retorna todos os registros da model
         """
         try:
-            return await self.__get_model_by_user().all()
+            query = self.get_model_by_user()
+            if self.search_field:
+                query = query.filter(**{f"{self.search_field}__icontains": search})
+            
+            return await self.query_paginator(query, page)
         except Exception as e:
             logger_error(__name__,e)
             raise HTTPException(status_code=400, detail=str(e))
@@ -52,7 +78,7 @@ class ViewController(BaseController):
             Record: Registro encontrado
         """
         try:
-            record = await self.model.filter(user_id=self.current_user.id).get_or_none(id=id)
+            record = await self.get_model_by_user().get_or_none(id=id)
             if not record:
                 raise HTTPException(status_code=404, detail="Registro não encontrado")
             return record
@@ -91,7 +117,7 @@ class ViewController(BaseController):
         """
         async with transactions.in_transaction() as transaction:
             try:
-                record = await self.__get_model_by_user().get_or_none(id=id)
+                record = await self.get_model_by_user().get_or_none(id=id)
                 if not record:
                     raise HTTPException(status_code=404, detail="Registro não encontrado")
                 await record.update(**params)
@@ -110,7 +136,7 @@ class ViewController(BaseController):
         """
         async with transactions.in_transaction() as transaction:
             try:
-                record = await self.__get_model_by_user().get_or_none(id=id)
+                record = await self.get_model_by_user().get_or_none(id=id)
                 model_name = record.__class__.__name__.lower()
                 model_folder = f"/app/files/{model_name}/{record.id}"
                 if not record:
