a2674f411dceb9eab02e8daa137b7b64d6737d4f
joaoser222 <joaoser222@gmail.com>
2025-04-12 16:59:32 -0300

    Corr. campos de search

--- api/app/models/search.py
+++ api/app/models/search.py
@@ -3,13 +3,13 @@ from .base import BaseModel
 
 class Search(BaseModel):
     user = fields.ForeignKeyField('models.User', related_name='searches')
-    original_name = fields.CharField(max_length=500)
+    name = fields.CharField(max_length=500)
     tolerance_level = fields.IntField()
-    file = fields.ForeignKeyField('models.File', related_name='searches')
+    photo = fields.ForeignKeyField('models.Photo', related_name='searches')
     collections = fields.JSONField(default=[])
 
     class Meta:
         table = "searches"
 
     def __str__(self):
-        return self.file_path
+        return self.name
