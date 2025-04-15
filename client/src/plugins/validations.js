8f51945cf001ab37b74e1304fa605907dc38022d
joaoser222 <joaoser222@gmail.com>
2025-04-14 21:41:14 -0300

    Adc. customização de mensagem de validação

--- client/src/plugins/validations.js
+++ client/src/plugins/validations.js
@@ -2,7 +2,7 @@
 export default {
   required: (value) => !!value || "Este campo é obrigatório",
   email: (value) =>(!value || /.+@.+\..+/.test(value)) || "Este campo deve ser um email válido",
-  minLength: (length) => (value) => value.length >= length || `Este campo deve ter pelo menos ${length} caracteres`,
-  maxLength: (length) => (value) => value.length <= length || `Este campo deve ter no máximo ${length} caracteres`,
+  minLength: (length, reference_name='caractere(s)') => (value) => value.length >= length || `Este campo deve ter pelo menos ${length} ${reference_name}`,
+  maxLength: (length, reference_name='caractere(s)') => (value) => value.length <= length || `Este campo deve ter no máximo ${length} ${reference_name}`,
   sameAs: (value,reference) => value === reference || "Os valores não coincidem"
 }
