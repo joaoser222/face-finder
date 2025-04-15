8cae2748a48456abc2c775da5b162ba534b43f0d
joaoser222 <joaoser222@gmail.com>
2025-04-15 19:58:22 -0300

    Adc. class contendo os status da coleção

--- api/app/models/collection.py
+++ api/app/models/collection.py
@@ -1,10 +1,16 @@
 from tortoise import fields, models
 from .base import BaseModel
+
+class CollectionStatus:
+    UNPACKING = 0
+    INDEXING = 1
+    FINISHED = 2
+
 class Collection(BaseModel):
     name = fields.CharField(max_length=100, unique=True)
     user = fields.ForeignKeyField('models.User', related_name='collections')
     thumbnail_photo = fields.ForeignKeyField('models.Photo', related_name='collections', null=True)
-    status = fields.IntField(default=0)
+    status = fields.IntField(default=CollectionStatus.UNPACKING)
     photo_quantity = fields.IntField(default=0)
 
     class Meta:
