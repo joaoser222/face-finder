815c97735ba64f63ba8626a321be36d2e2d4b7c7
joaoser222 <joaoser222@gmail.com>
2025-04-15 20:02:28 -0300

    Adc. tolerância mínima no campo

--- api/app/models/search.py
+++ api/app/models/search.py
@@ -5,10 +5,11 @@ class SearchStatus:
     WAITING = 0
     PROCESSING = 1
     FINISHED = 2
+
 class Search(BaseModel):
     user = fields.ForeignKeyField('models.User', related_name='searches')
     name = fields.CharField(max_length=500)
-    tolerance_level = fields.IntField()
+    tolerance_level = fields.IntField(default=60)
     status = fields.IntField(default=SearchStatus.WAITING)
     collections = fields.JSONField(default=[])
 
