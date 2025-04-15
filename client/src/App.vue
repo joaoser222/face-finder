5f87c4c8026c8f602332f8af1bb41f51ec34a275
joaoser222 <joaoser222@gmail.com>
2025-04-14 21:38:45 -0300

    Adc. exibição de dialog de carregamento

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,16 +4,21 @@
       <router-view />
     </v-main>
     <dialog-area ref="dialogArea"/>
+    <loading-dialog :dialog="loadingDialogStatus" :message="loadingDialogMessage"></loading-dialog>
   </v-app>
 </template>
 
 <script>
 import { ref, provide} from 'vue';
 import DialogArea from '@/components/DialogArea.vue';
+import LoadingDialog from '@/components/LoadingDialog.vue';
 export default {
-  components:{DialogArea},
+  components:{DialogArea,LoadingDialog},
   setup() {
     const dialogArea = ref(null);
+    const loadingDialogStatus = ref(false);
+    const loadingDialogMessage = ref('');
+
     const dialogCall = (options)=>{
       if (dialogArea.value) {
         dialogArea.value.showDialog(options);
@@ -23,8 +28,20 @@ export default {
         }, 100);
       }
     }
+
+    const loadingDialog = {
+      show: function(msg){
+        loadingDialogStatus.value = true;
+        loadingDialogMessage.value = msg || 'Carregando';
+      },
+      hide: function () {
+        loadingDialogStatus.value = false;
+        loadingDialogMessage.value = '';
+      }
+    }
     provide('dialog', dialogCall);
-    return { dialogArea };
+    provide('loadingDialog', loadingDialog);
+    return { dialogArea, loadingDialogStatus, loadingDialogMessage };
   }
 }
 </script>
---
+++ client/src/components/LoadingDialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-dialog
+      v-model="_dialog"
+      max-width="400"
+      persistent
+    >
+      <v-card height="250px" class="d-flex align-center justify-center pa-6">
+        <v-progress-circular class="my-6"></v-progress-circular>
+        <div class="pt-5 text-subtitle-1 font-weight-light">{{message}}</div>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script setup>
+  import { ref, watch } from 'vue';
+
+  // Recebendo a prop 'propValue' via props
+  const props = defineProps({
+    dialog: Boolean,  // ou outro tipo, como Number, Object, etc.
+    message: String
+  });
+
+  // Criando uma variável local baseada no valor da prop
+  const _dialog = ref(props.dialog);
+
+  // Watch para observar mudanças na prop e atualizar o valor local
+  watch(() => props.dialog, (newValue) => {
+    _dialog.value = newValue;
+  });
+
+</script>
