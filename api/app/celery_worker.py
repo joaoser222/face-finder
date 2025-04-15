bacf0a5951df2bc799164ee8e2a30619dfc4a16e
joaoser222 <joaoser222@gmail.com>
2025-04-15 19:57:09 -0300

    Adc. limitação de concorrência de tasks

--- api/app/celery_worker.py
+++ api/app/celery_worker.py
@@ -1,5 +1,5 @@
 from app.tasks import celery_app
 
 if __name__ == '__main__':
-    args = ['worker', '--loglevel=INFO']
+    args = ['--loglevel=INFO','--concurrency=5']
     celery_app.worker_main(argv=args)
