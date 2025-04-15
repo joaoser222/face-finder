4ff1b6ccf109bc821d8c0912f2b2801156381f80
joaoser222 <joaoser222@gmail.com>
2025-04-15 20:01:52 -0300

    Adc. class contendo os status de Pesquisa de imagens

--- api/app/models/search.py
+++ api/app/models/search.py
@@ -1,11 +1,15 @@
 from tortoise import fields, models
 from .base import BaseModel
 
+class SearchStatus:
+    WAITING = 0
+    PROCESSING = 1
+    FINISHED = 2
 class Search(BaseModel):
     user = fields.ForeignKeyField('models.User', related_name='searches')
     name = fields.CharField(max_length=500)
     tolerance_level = fields.IntField()
-    photo = fields.ForeignKeyField('models.Photo', related_name='searches')
+    status = fields.IntField(default=SearchStatus.WAITING)
     collections = fields.JSONField(default=[])
 
     class Meta:
