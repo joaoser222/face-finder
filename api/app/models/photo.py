42044a9a10d68699e31ce0606a3fdca4c14451ba
joaoser222 <joaoser222@gmail.com>
2025-04-15 19:57:38 -0300

    Rem. campos já presentes na class FileModel

--- api/app/models/photo.py
+++ api/app/models/photo.py
@@ -6,11 +6,6 @@ import mimetypes
 from .base import FileModel
 
 class Photo(FileModel):
-    original_name = fields.CharField(max_length=500)
-    file_path = fields.CharField(max_length=500)
-    extension_type = fields.CharField(max_length=100)
-    mime_type = fields.CharField(max_length=100)
-    size = fields.IntField()  # Tamanho em bytes
     user = fields.ForeignKeyField('models.User', related_name='photos')
     face_count = fields.IntField(default=0)
     is_indexed = fields.BooleanField(default=False) 
