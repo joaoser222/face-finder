cd380daae66e6590c92f53e627064acfd547f60f
joaoser222 <joaoser222@gmail.com>
2025-04-16 22:12:13 -0300

    Rem. delete cascade de thumbnail

--- api/app/models/collection.py
+++ api/app/models/collection.py
@@ -9,7 +9,7 @@ class CollectionStatus:
 class Collection(BaseModel):
     name = fields.CharField(max_length=100, unique=True)
     user = fields.ForeignKeyField('models.User', related_name='collections')
-    thumbnail_photo = fields.ForeignKeyField('models.Photo', related_name='collections', null=True)
+    thumbnail_photo = fields.ForeignKeyField('models.Photo', related_name='collections', null=True, on_delete=fields.SET_NULL)
     status = fields.IntField(default=CollectionStatus.UNPACKING)
     photo_quantity = fields.IntField(default=0)
 
