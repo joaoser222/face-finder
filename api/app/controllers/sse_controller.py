abcab4207f003f12207d7dc7f7b847293bd751b2
joaoser222 <joaoser222@gmail.com>
2025-04-18 19:42:55 -0300

    Corr. recepção de eventos SSE

--- api/app/controllers/sse_controller.py
+++ api/app/controllers/sse_controller.py
@@ -11,25 +11,23 @@ class SSEController(BaseController):
     def __init__(self):
         super().__init__()
         
-        # Rota para eventos SSE que requer autenticação
         self.router.add_api_route("/events/{token}", self.events, methods=["GET"])
 
-    async def events(self,token):
+    async def events(self, token):
         """
         Endpoint para iniciar uma conexão SSE.
-        O usuário já está disponível em self.current_user devido ao Depends(self.set_current_user)
-        
-        Returns:
-            Um StreamingResponse que mantém a conexão aberta
         """
 
         session = await Session.filter(token=token).first()
         if not session:
             raise HTTPException(status_code=401, detail="Sessão não encontrada")
         
-        # Iniciar stream de eventos
+        async def event_stream():
+            async for event in sse_manager.get_event_generator(session.user_id):
+                yield f"data: {json.dumps(event)}\n\n"
+
         return StreamingResponse(
-            sse_manager.get_event_generator(session.user_id),
+            event_stream(),
             media_type="text/event-stream",
             headers={
                 "Cache-Control": "no-cache",
