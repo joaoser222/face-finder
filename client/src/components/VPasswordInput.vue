3ae4487d8777a6e4de4c7ce3cd12b86ec7bc7c9a
joaoser222 <joaoser222@gmail.com>
2025-04-18 11:30:57 -0300

    Corr. bug de botão de exibição de senha

--- client/src/components/VPasswordInput.vue
+++ client/src/components/VPasswordInput.vue
@@ -9,45 +9,62 @@
     <template #append-inner>
       <v-btn 
         :icon="showPassword ? 'Eye' : 'EyeOff'" 
-        tabindex="-1"
+        tabindex="-1" 
         variant="text"
         @click="togglePasswordVisibility"
         rounded="circle"
         size="small"
         :style="{fontSize: '16px'}"
-      />
+      >
+      </v-btn>
     </template>
   </v-text-field>
 </template>
 
-<script setup>
-import { ref, watch } from 'vue'
+<script>
+import { ref, watch } from 'vue';
 
-const props = defineProps({
-  modelValue: {
-    type: String,
-    default: ''
-  }
-})
+export default {
+  name: 'VPasswordInput',
+  inheritAttrs: false, // Evita que os atributos sejam aplicados ao elemento raiz
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+  },
+  emits: ['update:modelValue', 'input'], // Declara os eventos que serão emitidos
+  setup(props, { emit }) {
+    const internalValue = ref(props.modelValue);
+    const showPassword = ref(false);
 
-const emit = defineEmits(['update:modelValue', 'input'])
+    // Atualiza o valor interno quando a prop `modelValue` muda
+    watch(
+      () => props.modelValue,
+      (newValue) => {
+        internalValue.value = newValue;
+      }
+    );
 
-const internalValue = ref(props.modelValue)
-const showPassword = ref(false)
+    const togglePasswordVisibility = () => {
+      showPassword.value = !showPassword.value;
+    };
 
-watch(() => props.modelValue, (newValue) => {
-  internalValue.value = newValue
-})
+    const onInput = (event) => {
+      emit('input', event); // Reemite o evento `input`
+    };
 
-const togglePasswordVisibility = () => {
-  showPassword.value = !showPassword.value
-}
+    const onUpdateModelValue = (value) => {
+      emit('update:modelValue', value); // Reemite o evento `update:modelValue`
+    };
 
-const onInput = (event) => {
-  emit('input', event)
-}
-
-const onUpdateModelValue = (value) => {
-  emit('update:modelValue', value)
-}
+    return {
+      internalValue,
+      showPassword,
+      togglePasswordVisibility,
+      onInput,
+      onUpdateModelValue,
+    };
+  },
+};
 </script>
