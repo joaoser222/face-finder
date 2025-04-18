93b93a1b44f37066924f1a4e3a2de82b740bdccd
joaoser222 <joaoser222@gmail.com>
2025-04-18 20:14:27 -0300

    Adc. rota para pesquisa de coleções

--- api/app/controllers/search_controller.py
+++ api/app/controllers/search_controller.py
@@ -3,8 +3,9 @@ from app.models.search_face import SearchFace
 from .view_controller import ViewController
 from app.models.photo import Photo
 from app.models.job import Job
+from app.models.collection import Collection,CollectionStatus
 from tortoise import transactions
-from fastapi import UploadFile, HTTPException, Form, Depends
+from fastapi import UploadFile, HTTPException, Form, Query, Depends
 from fastapi.responses import JSONResponse
 import json,os
 from app.utils import logger_info,logger_error
@@ -17,6 +18,13 @@ class SearchController(ViewController):
 
     def __init__(self):
         super().__init__()
+
+        self.router.add_api_route(
+            "/collections",
+            self.get_collections,
+            methods=["GET"],
+            dependencies=[Depends(self.set_current_user)]
+        )
             
     async def create(
         self,
@@ -66,6 +74,51 @@ class SearchController(ViewController):
         except Exception as e:
             logger_error(__name__, e)
             raise HTTPException(400, str(e))
+    
+    async def get_collections(
+        self,
+        page: int = Query(1, ge=1, description="Número da página"),
+        search: str = Query(None, description="Pesquisa")
+    ):
+        """
+        Retorna fotos paginadas de uma collection
+        
+        Args:
+            page (int): Número da página (default: 1)
+            search (str, optional): Texto de pesquisa
+        
+        Returns:
+            dict: {
+                "items": lista de fotos,
+                "total": total de fotos,
+                "page": página atual,
+                "per_page": itens por página,
+                "total_pages": total de páginas
+            }
+        """
+        try:
+            filter_search=""
+            if self.search_field and search:
+                filter_search = f" AND collections.name LIKE '%%{search}%%'"
+
+            raw_query = f"""
+                SELECT collections.* FROM collections
+                WHERE
+                    collections.user_id = {self.current_user.id} AND
+                    collections.status = {CollectionStatus.FINISHED} AND
+                    EXISTS (
+                        SELECT 1 FROM faces
+                        INNER JOIN photos ON photos.owner_id=collections.id
+                        WHERE
+                            photos.owner_type='collection' AND
+                            faces.photo_id = photos.id
+                    )
+                    {filter_search}
+            """
+            return await self.query_paginator(raw_query, page)
+        except Exception as e:
+            logger_error(__name__,e)
+            raise HTTPException(status_code=400, detail=str(e))
     
     async def update(self, id: int, params: dict):
         """
